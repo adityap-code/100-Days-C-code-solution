fb2367d87dc7143df54f459054380c3466d70027
adityap-code <Aditya.40695@stu.upes.ac.in>
2026-08-31 14:50:54 +0530

    add

--- c34.c
+++ c34.c
@@ -1,4 +1,4 @@
-/* Write a program to find the 1’s complement of a binary number and print it.
+//Write a program to find the 1’s complement of a binary number and print it.
 
 #include <stdio.h>
 
@@ -7,31 +7,5 @@ int main () {
     printf("ENTER NUMBER : ");
     scanf("%d",&a);
     
-    return 0;
-}*/
-
-#include <stdio.h>
-
-int main() 
-{   int n;
-    printf("enter number : ");
-    scanf("%d",&n);
-    
-    if (n<=1)
-    {
-        printf(" not a prime number");
-    }
-    else {
-        for (int i=2 ; i<n ; i++){
-            if (n%i==0){
-                printf("not a prime number\n");        
-            }
-            else {
-                printf("prime number\n ");
-            }         
-        }
-    }
-    
-
     return 0;
 }
